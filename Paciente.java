e15ff99013acdec881936e1bbb782e371df76792
22 <22@192.168.0.17>
2020-11-09 20:14:26 -0300

    implemente la clase pacientes

--- Paciente.java
+++ Paciente.java
@@ -1,29 +1,88 @@
 
-
-/**
- * @author Nicolas Jimenez
- * @version 1.0
- * @created 03-nov.-2020 09:21:53
- */
 public class Paciente {
-
-	private String apellido;
+	
+	private String nombre ;
+	private  String apellido; 
 	private int dni;
-	private String domicilio;
-	private String nombre;
+	private String direccion;
 	private Turno turnos;
-	private Turno m_Turno;
-
-	public Paciente(){
-
+	
+	
+	
+	public Paciente(String nombre, String apellido, int dni, String direccion, Turno turnos) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.direccion = direccion;
+		this.turnos = turnos;
 	}
 
-	public void finalize() throws Throwable {
 
+
+	public String getNombre() {
+		return nombre;
 	}
 
-	public boolean abonarPrestacion(){
-		return false;
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+
+	public int getDni() {
+		return dni;
+	}
+
+
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+
+	public Turno getTurnos() {
+		return turnos;
+	}
+
+
+
+	public void setTurnos(Turno turnos) {
+		this.turnos = turnos;
+	}
+	
+	
+	public boolean abonaPrestacion() {
+		
+	return false;
+		
 	}
 
 }
